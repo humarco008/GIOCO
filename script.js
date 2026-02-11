896bafe4f71137189743adb4abe65f14d1bfca58
humarco008 <hu.marco@iisgalvanimi.edu.it>
2026-02-11 13:50:10 +0000

    punto 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function startGame() {
     myGameArea.start();
-}
+    myGameArea.draw(redSquare);
 
+}
+    var redSquare = {
+  width: 20,
+  height: 20,
+  x: 10,
+  y: 120,
+  color: "red"
+};
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -9,7 +17,28 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
+    },
+     draw: function(component) {
+    this.context.fillStyle = component.color;
+    this.context.fillRect(component.x, component.y, component.width, component.height);
+  }
+
 }
+function moveup() {
+  redSquare.y -= 30;
+}
+
+function movedown() {
+  redSquare.y += 30;
+}
+
+function moveleft() {
+  redSquare.x -= 30;
+}
+
+function moveright() {
+  redSquare.x += 30;
+}
+
 
 
